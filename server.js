25513f0cf67ba2f5a748914f38c8781114a4360b
Serguei Rodriguez <sjosevich@gmail.com>
2020-01-27 19:56:16 -0700

    Added the user_route file in order to validate the username, email and password of the users

--- server.js
+++ server.js
@@ -43,6 +43,24 @@ app.use(passport.initialize())
 app.use(passport.session())
 app.use(methodOverride('_method'))
 
+// User Route
+app.use('/user', require('./routes/user_routes.js'));
+
+// catch 404 and forward to error handler
+// app.use(function(req, res, next){
+//   next(createError(404));
+// })
+
+//error handler
+app.use(function(err,req,res,next){
+  //set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err: {};
+
+  //render the error page
+  res.status(err.status || 500);
+})
+
 app.get('/', checkNotAuthenticated, function(req, res){
   res.render('index.handlebars')
 })
