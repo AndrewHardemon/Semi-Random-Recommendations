017374798019d5999c8af4bb7f16300802887c41
Serguei Rodriguez <sjosevich@gmail.com>
2020-01-31 11:01:24 -0700

    Added the validation.js file in order to validate my DB after the Post request

--- models/index.js
+++ models/index.js
@@ -6,6 +6,7 @@ var Sequelize = require("sequelize");
 var basename = path.basename(module.filename);
 var env = process.env.NODE_ENV || "development";
 var config = require(__dirname + "/../config/config.js")[env];
+//console.log(config)
 var db = {};
 
 if (config.use_env_variable) {
@@ -38,5 +39,7 @@ Object.keys(db).forEach(function(modelName) {
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
+
+
 
 module.exports = db;
