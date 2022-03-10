61302153b9c5873041ca24e2e2bebc9b26bc8db2
AeroAtlas <andrewhardemon@gmail.com>
2022-03-10 02:02:55 -0700

    modified url to include rating fixed issue with video not being removed when request is resubmitted finished with movie search

--- public/js/app.js
+++ public/js/app.js
@@ -117,6 +117,7 @@ $("#submit").on("click", async function (event) {
     trailer.attr("src", ytVid)
     trailerDiv.append(trailer);
     console.log(trailerDiv[0])
+    $("#videoOutput").html("")
     $("#videoOutput").append(trailerDiv[0])
 
 
